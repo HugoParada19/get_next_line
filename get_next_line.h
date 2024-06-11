268d3befe67cf9c763f38c1cd730ba98c3bfdf19
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-06-11 16:59:27 +0100

    wtf

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 16:39:49 by htrindad          #+#    #+#             */
+/*   Updated: 2024/06/11 16:44:31 by htrindad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+void	ft_bzero(char *str, size_t size);
+
+#endif
