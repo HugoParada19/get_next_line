7de97e35ee702b0ed74dfd91bc4b5646b0f1b6fc
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-06-11 15:56:22 +0100

    redo

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 15:55:25 by htrindad          #+#    #+#             */
+/*   Updated: 2024/06/11 15:56:20 by htrindad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
